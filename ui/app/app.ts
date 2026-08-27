7c57e52bc7d2c2e989d17eba1e32591e48143254
Jose Luna <sema.lunap@gmail.com>
2026-08-27 06:55:53 +0000

    fix: broken asset loading in github pages

--- ui/app/app.ts
+++ ui/app/app.ts
@@ -6,10 +6,16 @@ import loadInitializers from 'ember-load-initializers';
 import config from 'ui/config/environment';
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
+import { setBuildURLConfig } from '@warp-drive/utilities';
 
 if (macroCondition(isDevelopingApp())) {
   importSync('./deprecation-workflow');
 }
+
+setBuildURLConfig({
+  host: null,
+  namespace: import.meta.env.BASE_URL.substring(1) + 'srd/spells',
+});
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
--- ui/app/routes/spell-area-calc.ts
+++ ui/app/routes/spell-area-calc.ts
@@ -1,13 +1,14 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import { type Store } from '@warp-drive/core';
+import { buildBaseURL } from '@warp-drive/utilities';
 
 export default class SpellAreaCalcRoute extends Route {
   @service declare store: Store;
 
   async model() {
     return this.store.request({
-      url: '/srd/spells/fire-bolt.json',
+      url: buildBaseURL({ resourcePath: 'fire-bolt.json' }),
       method: 'GET',
     });
   }
