3df193e9536badc3ca2af98a0f6b182a9314d374
Jose Luna <sema.lunap@gmail.com>
2026-08-27 14:10:27 +0000

    ci: add husky, conventional commits, fix some linting configurations

--- ui/.prettierrc.mjs
+++ ui/.prettierrc.mjs
@@ -2,7 +2,7 @@ export default {
   plugins: ['prettier-plugin-ember-template-tag'],
   overrides: [
     {
-      files: '*.{js,gjs,ts,gts,mjs,mts,cjs,cts}',
+      files: '*.{js,gjs,ts,gts,mjs,mts,cjs,cts,json}',
       options: {
         singleQuote: true,
         templateSingleQuote: false,
